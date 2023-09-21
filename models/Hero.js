b20d16c25ef102af976a740da41d5ace2f445c0f
Mariyan Uzunov <themariyanuzunov@gmail.com>
2023-09-21 20:21:38 +0300

    hero extra abilities

--- models/Hero.js
+++ models/Hero.js
@@ -6,6 +6,7 @@ const heroSchema = new mongoose.Schema(
     name: { type: String, required: true },
     race: { type: mongoose.Schema.Types.ObjectId, ref: 'HeroRace', required: true },
     avatar: { type: String, required: true },
+    extraAbilities: [{ type: String }],
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
   },
   {
